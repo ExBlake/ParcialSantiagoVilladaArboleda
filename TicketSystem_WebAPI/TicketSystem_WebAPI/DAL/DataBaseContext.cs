ef9e00b7aa24124bbc8fbfe23495f066a0b555c1
villa <villa@TIAGO>
2023-05-21 16:35:03 -0500

    DataBaseContext.cs class created and appsettings.json modified

---
+++ TicketSystem_WebAPI/TicketSystem_WebAPI/DAL/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using TicketSystem_WebAPI.DAL.Entities;
+
+namespace TicketSystem_WebAPI.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options){}
+        public DbSet<Tickets> Tickets { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Tickets>().HasIndex(t => t.Id).IsUnique();
+        }
+    }
+}
--- TicketSystem_WebAPI/TicketSystem_WebAPI/DAL/Entities/Tickets.cs
+++ TicketSystem_WebAPI/TicketSystem_WebAPI/DAL/Entities/Tickets.cs
@@ -1,6 +1,21 @@
-﻿namespace TicketSystem_WebAPI.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketSystem_WebAPI.DAL.Entities
 {
     public class Tickets
     {
+        [Key]
+        [Display(Name = "Id")] //Show
+        [Required(ErrorMessage = "The field {0} is required")] //show message error, not null
+        public Guid Id { get; set; }
+
+        [Display(Name = "Usedate")]
+        public DateTime? UseDate { get; set; }
+
+        [Display(Name = "It's used?")]
+        public bool IsUsed { get; set; }
+
+        [Display(Name = "Entrance Gate")]
+        public string EntranceGate { get; set; }
     }
 }
