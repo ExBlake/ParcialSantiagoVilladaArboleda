e6648538bc5b6fa1fa02c1a260e7a1bfe7c459eb
villa <villa@TIAGO>
2023-05-21 17:26:44 -0500

    new Class SeederDB.cs created

---
+++ TicketSystem_WebAPI/TicketSystem_WebAPI/DAL/SeederDB.cs
@@ -0,0 +1,30 @@
+﻿namespace TicketSystem_WebAPI.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();//This line helps me create my database automatically
+            await PopulateTicketsAsync();
+            await _context.SaveChangesAsync();//Save the database
+        }
+
+        private async Task PopulateTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for(int i = 0; i <= 50000; i++)
+                {
+                    _context.Tickets.Add(new Entities.Tickets { UseDate = null, IsUsed = false, EntranceGate = null });
+                }
+            }
+        }
+    }
+}
