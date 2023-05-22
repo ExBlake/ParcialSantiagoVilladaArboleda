643643a2abde10513ffbef6393124a1d450f25d2
villa <villa@TIAGO>
2023-05-22 14:51:42 -0500

    Finish

--- TicketSystem_WebPages/Controllers/TicketsController.cs
+++ TicketSystem_WebPages/Controllers/TicketsController.cs
@@ -18,66 +18,66 @@ namespace TicketSystem_WebPages.Controllers
         
         public async Task<IActionResult> Index()
         {
-            var url = "https://localhost:7042/EditTicket/";
+            var url = "https://localhost:7042/Get";
             var json = await _httpClient.CreateClient().GetStringAsync(url);
             List<Tickets> tickets = JsonConvert.DeserializeObject<List<Tickets>>(json);
             return View(tickets);
         }
 
-        [HttpPost, ActionName("EditTicket")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditTicket(Guid? id, Tickets tickets)
-        {
+        //[HttpPost, ActionName("EditTicket")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> EditTicket(Guid? id, Tickets tickets)
+        //{
             
-            var url = String.Format("https://localhost:7042/EditTicket/{0}", id);
-            var json = await _httpClient.CreateClient().PutAsJsonAsync(url, tickets);
-            return View(tickets);
-        }
+        //    var url = String.Format("{0}", id);
+        //    var json = await _httpClient.CreateClient().PutAsJsonAsync(url, tickets);
+        //    return View(tickets);
+        //}
 
         
-        [HttpGet]
-        public IActionResult Valide()
-        {
-            return View();
-        }
+        //[HttpGet]
+        //public IActionResult Valide()
+        //{
+        //    return View();
+        //}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> TicketEdit(Guid? id, Tickets tickets)
-        {
-            var url = String.Format("https://localhost:7042/EditTicket/{0}", id);
-            var json = await _httpClient.CreateClient().GetStringAsync(url);
-            Tickets ticket = JsonConvert.DeserializeObject<Tickets>(json);
-            return View(ticket);
-            //if (tickets.Id == id)
-            //{
-            //    if (tickets.IsUsed == false)
-            //    {
-            //        try
-            //        {
-            //            tickets.UseDate = DateTime.Now;
-            //            tickets.IsUsed = true;
-            //            tickets.EntranceGate = "";
-
-
-            //            return View(tickets);
-            //        }
-            //        catch (Exception e)
-            //        {
-            //            return Conflict(e.Message);
-            //        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> TicketEdit(Guid? id, Tickets tickets)
+        //{
+        //    var url = String.Format("{0}", id);
+        //    var json = await _httpClient.CreateClient().GetStringAsync(url);
+        //    Tickets ticket = JsonConvert.DeserializeObject<Tickets>(json);
+        //    return View(ticket);
+        //    //if (tickets.Id == id)
+        //    //{
+        //    //    if (tickets.IsUsed == false)
+        //    //    {
+        //    //        try
+        //    //        {
+        //    //            tickets.UseDate = DateTime.Now;
+        //    //            tickets.IsUsed = true;
+        //    //            tickets.EntranceGate = "";
 
 
+        //    //            return View(tickets);
+        //    //        }
+        //    //        catch (Exception e)
+        //    //        {
+        //    //            return Conflict(e.Message);
+        //    //        }
 
 
 
-            //    }
 
-            //    return Conflict("Boleta ya usada");
 
-            //}
-            //return NotFound("Boleta no válida");
-        }
+        //    //    }
+
+        //    //    return Conflict("Boleta ya usada");
+
+        //    //}
+        //    //return NotFound("Boleta no válida");
+        //}
 
         
         
