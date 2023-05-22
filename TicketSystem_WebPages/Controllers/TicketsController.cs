474660f5ee08aff5183f8f6f9e9990a5f0ccd398
villa <villa@TIAGO>
2023-05-22 09:36:14 -0500

    Class TicketsController.cs created and index.cshtml Created on view

--- TicketSystem_WebPages/Controllers/TicketsController.cs
+++ TicketSystem_WebPages/Controllers/TicketsController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TicketSystem_WebAPI.DAL.Entities;
+using Newtonsoft.Json;
 
 namespace TicketSystem_WebPages.Controllers
 {
     public class TicketsController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClient;
+        public TicketsController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
         {
             var url = "https://localhost:7042/GeTicketsById/";
-            var json = "";
-            return View();
+            var json = await _httpClient.CreateClient().GetStringAsync(url);
+            List<Tickets> tickets = JsonConvert.DeserializeObject<List<Tickets>>(json);
+            return View(tickets);
         }
     }
 }
